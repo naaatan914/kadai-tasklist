f4037e6f8272746759505e1035d1a0b8b49e59d2
Narumi Yuki <narumi.y.yuki@gmail.com>
2017-04-24 08:24:49 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,8 +42,9 @@ class TasksController < ApplicationController
   
   def set_task
     @task = Task.find(params[:id])
+  end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
